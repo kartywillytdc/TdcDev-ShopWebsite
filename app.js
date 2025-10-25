040030cbc9dc7e790e8e2b61d8be2ccfb951a9d7
kartywillytdc <kartywillytdc@gmail.com>
2025-10-25 13:55:39 -0300

    Update and rename deepseek_javascript_20251025_46a7ea.js to app.js

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ import {
 
 // Firebase Configuration - SUBSTITUA COM SUAS CONFIGURAÇÕES
 const firebaseConfig = {
-    apiKey: "SUA_API_KEY",
-    authDomain: "SEU_PROJETO.firebaseapp.com",
-    projectId: "SEU_PROJETO_ID",
-    storageBucket: "SEU_PROJETO.appspot.com",
-    messagingSenderId: "SEU_SENDER_ID",
-    appId: "SEU_APP_ID"
+  apiKey: "AIzaSyBwjwP1Ewx4Z2wRS_Xt14B0DRTrM8x6WOg",
+  authDomain: "tdc-dev-web.firebaseapp.com",
+  projectId: "tdc-dev-web",
+  storageBucket: "tdc-dev-web.firebasestorage.app",
+  messagingSenderId: "848965120165",
+  appId: "1:848965120165:web:0c69411e01f282954e9d4c",
 };
 
 // Initialize Firebase
